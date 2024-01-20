a6c27251b5b4706dbb1911f9798dd4a1a6a7710f
FloppyDisk <github@flpydsk.dev>
2024-01-20 19:54:38 +1300

    Add some options

--- quick_tunnel.sh
+++ quick_tunnel.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
-
 # Licensed under the Unlicense.
 # FloppyDisk 2024, https://github.com/flpydsk/quick_tunnel.git
 
-edge_ip_version="6"
-if [[ "$1" == "4" ]]
+tunnel_options+=($@)
+if [[ "${#tunnel_options[@]}" == "0" ]]
 then
-  edge_ip_version="4"
+  tunnel_options+=("--hello-world")
+fi
+
+if [[ "$tunnel_options" != *"--edge-ip-version"* ]]
+then
+  tunnel_options+=("--edge-ip-version" "6")
 fi
 
 quick_tunnel="$(curl --silent https://api.trycloudflare.com/tunnel -X POST -H "Content-Type: application/json")"
 tunnel_token="$(echo "$quick_tunnel" | jq '{a: .result.account_tag, t: .result.id, s: .result.secret}' -c | base64 -w0)"
-echo -e "\n\n\nHostname: $(echo "$quick_tunnel" | jq '.result.hostname' -r)\n\n\n"
-cloudflared tunnel --edge-ip-version "$edge_ip_version" --hello-world  run --token "$tunnel_token"
+tunnel_host="$(echo "$quick_tunnel" | jq '.result.hostname' -r)"
+
+echo -e "\n\n\nHost  : $tunnel_host\nToken : $tunnel_token\n\n\n"
+
+cloudflared tunnel ${tunnel_options[@]} run --token "$tunnel_token"
